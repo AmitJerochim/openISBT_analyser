d365f6e5223fee568781177f96c38b56762d74a3
amit <amit.jerochim@gmx.de>
2020-06-08 13:02:18 +0200

    added comments for quick start deployments

--- main.sh
+++ main.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-
+#
+#setup the path for a directory containing openAPI 3.0 files in json format.
+#	don't use slash at the end of the file: ../oasFiles instead of ../oasFiles/
+#
 OAS_FILES=../openapi-data/oasFilesJson
+
+#
+#setup the path for a directory where your Logging files should be saved
+#	don't use slash at the end of the file: ../logFiles instead of ../logFiles/
+#
 LOG_FILES_DIRECTORY=./log_files
+
+#
+#setup the path for a pattern configuration file
+#
 PATTERN_CONFIG_FILE=../openISBT/resources/patternConfigs/amitExperiment.json
+
+#
+#setup the path for openISBT root directory
+#
 OPENISBT_ROOT_DIRECTORY=../openISBT
 
 ./run_openisbt.sh --log-files-directory $LOG_FILES_DIRECTORY --oas-files-directory $OAS_FILES --openisbt-root-directory  $OPENISBT_ROOT_DIRECTORY --pattern-config-file $PATTERN_CONFIG_FILE
