224c76121f4271ba63bceb530c2cec23e6ef37a9
amit <amit.jerochim@gmx.de>
2020-06-12 23:19:38 +0200

    add cleaning methods to the top of file

--- main.sh
+++ main.sh
@@ -17,16 +17,22 @@ LOG_FILES_DIRECTORY=./log_files
 PATTERN_CONFIG_FILE=../openISBT/resources/patternConfigs/amitExperiment.json
 
 #
-#setup the path for openISBT root directory
+#setup the path for openISBT matching tool jar
 #
 MATCHING_TOOL_JAR=../openISBT/openISBTBackend/build/libs/matchingTool-1.0-SNAPSHOT-all.jar 
 
-./run_openisbt.sh --log-files-directory $LOG_FILES_DIRECTORY --oas-files-directory $OAS_FILES_DIRECTORY --matching-tool-jar $MATCHING_TOOL_JAR --pattern-config-file $PATTERN_CONFIG_FILE
 
-./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-corrupt-files
-./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-petstore-apis
-./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-iot-apis
-./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-oauth-apis
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-short-files
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-insufficient-oas-files
 
-#./analyse_apis.sh  --directory $LOG_FILES_DIRECTORY --determine-full-supported-apis
+
+#./run_openisbt.sh --log-files-directory $LOG_FILES_DIRECTORY --oas-files-directory $OAS_FILES_DIRECTORY --matching-tool-jar $MATCHING_TOOL_JAR --pattern-config-file $PATTERN_CONFIG_FILE
+
+
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-corrupt-files
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-petstore-apis
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-iot-apis
+#./clean_data.sh --oas-files-directory $OAS_FILES_DIRECTORY --log-files-directory $LOG_FILES_DIRECTORY --remove-oauth-apis
+
+./analyse_apis.sh  --directory $LOG_FILES_DIRECTORY --determine-full-supported-apis
 #./analyse_apis.sh  --directory $LOG_FILES_DIRECTORY --determine-supported-operations
