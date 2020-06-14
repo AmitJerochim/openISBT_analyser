fed990c44a9549b17f0d9f55a63c36a16d455a88
amit <amit.jerochim@gmx.de>
2020-06-14 03:50:45 +0200

    script logs csv containing also aggregation of sub-resources

--- analyse_corrupt_files.sh
+++ analyse_corrupt_files.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 OAS_FILES_DIRECTORY="../openapi-data/oasFiles.bak"
 
-echo $OAS_FILES_DIRECTORY
 OAS_FILES_DIRECTORY=$( echo $OAS_FILES_DIRECTORY | sed 's#/#\\/#g')
-echo $OAS_FILES_DIRECTORY
 less errors.log | grep "Exception occured" | sed 's/Exception occured while processing file:\t //'| sed  "s/^/$OAS_FILES_DIRECTORY\//" >corrupt_files.txt
+ALL_SUBRESOURCE_OPERATIONS=0
+ALL_TOPLEVEL_OPERATIONS=0
 ALL_OPERATIONS=0
+	echo -e "Toplevel-Operations;Subresource-Operations;Sum-Toplevel-Operations;Sum-Subresource-Operations;Sum-All-Operations;Current-OAS-File"
 cat corrupt_files.txt  | while read line
 do 
-#	echo ${line##*/}
-	OPERATIONS=$( ./helper_functions.sh --count-available-operations -f $line )
-	ALL_OPERATIONS=$((ALL_OPERATIONS+OPERATIONS))
-	echo -e "Operations: \t $OPERATIONS \t allOperations: \t $ALL_OPERATIONS \t Current: \t ${line##*/}" 
+	TOPLEVEL_OPERATIONS=$( ./helper_functions.sh --count-available-operations -f $line )
+	SUBRESOURCE_OPERATIONS=$( node oas_reader.js $line 'true' |  head -n 1 |sed 's/Available Operations:\t//'   )
+	ALL_SUBRESOURCE_OPERATIONS=$((ALL_SUBRESOURCE_OPERATIONS+SUBRESOURCE_OPERATIONS))
+	ALL_TOPLEVEL_OPERATIONS=$((ALL_TOPLEVEL_OPERATIONS+TOPLEVEL_OPERATIONS))
+	ALL_OPERATIONS=$((ALL_OPERATIONS+TOPLEVEL_OPERATIONS+SUBRESOURCE_OPERATIONS))
+	echo -e "$TOPLEVEL_OPERATIONS;$SUBRESOURCE_OPERATIONS;$ALL_TOPLEVEL_OPERATIONS;$ALL_SUBRESOURCE_OPERATIONS;$ALL_OPERATIONS;${line##*/}" 
 done
 rm corrupt_files.txt
-echo $ALL_OPERATIONS
